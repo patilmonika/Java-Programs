b25ee9eeaa7d301d75be9f366f86df7d434efb36
patilmonika <50772846+patilmonika@users.noreply.github.com>
2021-12-30 23:05:53 +0530

    Add files via upload

---
+++ Quationt.java
@@ -0,0 +1,17 @@
+//2. Java Program to Find Quotient and Remainder
+
+package lesson1;
+
+public class Quationt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1=15, n2=2;
+		int quotient = n1/n2;
+		int remainder = n1 % n2;
+		
+		System.out.println("Quotient is :"+ quotient);
+		System.out.println("Reminder is :" + remainder);
+
+}
+}
