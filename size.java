03080528b0486961e40f2e32cd501d28f426be35
patilmonika <50772846+patilmonika@users.noreply.github.com>
2021-12-31 22:13:22 +0530

    Add files via upload

---
+++ size.java
@@ -0,0 +1,15 @@
+//3.Java Program to Find Size of int, float, double and char in Your System
+
+package lesson1;
+
+public class size {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Size of int is :" + (Integer.SIZE/8) + "bytes.");
+		System.out.println("Size of int Float is :" + (Float.SIZE/8) + "bytes.");
+		System.out.println("Size of int Double is:" + (Double.SIZE/8) + "bytes.");
+		System.out.println("Size of Char is :" + (Character.SIZE/8) + "bytes.");
+	}
+
+}
