e6c7c0682ae7948f88d351fa1539a5b6f9925677
patilmonika <50772846+patilmonika@users.noreply.github.com>
2021-12-31 22:13:56 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,18 @@
+//5.Java Program to Check Whether Number is Even or Odd
+package lesson1;
+
+public class even {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 3;
+		if (num % 2 == 0) {
+			System.out.println("Entered number is even ");
+			
+		}
+		else {
+			System.out.println("Enterd number is odd");
+		}
+	}
+
+}
---
+++ swaping.java
@@ -0,0 +1,21 @@
+//4.Java Program to Swap Two Numbers
+
+package lesson1;
+
+public class swaping {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1 = 10, n2 = 5, num;
+		//Before swapping 
+		System.out.println("Before swapping:= "+n1 + " " +n2);
+		num = n1;
+		n1 = n2;
+		n2 = num ;
+		
+		System.out.println("Swaping of two noumbers are := " + n1+" " + n2);
+		
+		
+	}
+
+}
